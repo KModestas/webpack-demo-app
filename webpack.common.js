c43be08bf344a0d9b455d9ba781161b82f2a07dd
Modestas <modestask97@gmail.com>
2020-02-06 13:23:22 +0000

    TerserPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,27 +1,29 @@
-const path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: {
-    main: "./src/index.js",
-    vendor: "./src/vendor.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: ["html-loader"]
-      },
-      {
-        test: /\.(svg|png|jpg|gif)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[hash].[ext]",
-            outputPath: "imgs"
-          }
-        }
-      }
-    ]
-  }
-};
+	entry: {
+		main: './src/index.js',
+		vendor: './src/vendor.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: ['html-loader']
+			},
+			{
+				// whenever you encounter an img ending with... use file loader and add those images to your [dist] folder
+				test: /\.(svg|png|jpg|gif)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name].[hash].[ext]',
+						// imgs will be stored in an imgs directroy within your dist folder
+						outputPath: 'imgs'
+					}
+				}
+			}
+		]
+	}
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const merge = require('webpack-merge')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+// this plugin comes automatically
 const TerserPlugin = require('terser-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
@@ -12,6 +13,7 @@ module.exports = merge(common, {
 	mode: 'production',
 	output: {
 		// which file to bundle your code in
+		// content hash generates a new file name every time your file changes so that your browser doesnt cache the file and load outdated unchanged code
 		filename: '[name].[contentHash].bundle.js',
 		// tells webpack while folder to bundle your code in
 		// dirname is a variable that coreespondes to your machine e.g. users/modestas/development etc
@@ -20,9 +22,11 @@ module.exports = merge(common, {
 	optimization: {
 		minimizer: [
 			new OptimizeCssAssetsPlugin(),
+			// need to add this plugin back (minifies js) in which is used by default but overridden if you add args to minimizer
 			new TerserPlugin(),
 			new HtmlWebpackPlugin({
 				template: './src/template.html',
+				// pass in extra paramteres to minify your html
 				minify: {
 					removeAttributeQuotes: true,
 					collapseWhitespace: true,
@@ -40,6 +44,7 @@ module.exports = merge(common, {
 			{
 				test: /\.scss$/,
 				use: [
+					//  use MiniCssExtractPlugin instead of style loader in production
 					MiniCssExtractPlugin.loader, //3. Extract css into files
 					'css-loader', //2. Turns css into commonjs
 					'sass-loader' //1. Turns sass into css
